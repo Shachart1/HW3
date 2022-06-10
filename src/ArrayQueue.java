7dd11bccf117f9b1618f8a6f7ec795ef1f9fd156
MickeyLermanTechnion <102416767+MickeyLermanTechnion@users.noreply.github.com>
2022-06-10 16:23:54 +0300

    mickey

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -2,19 +2,20 @@ import java.util.Iterator;
 
 public class ArrayQueue<E extends Cloneable> implements Queue{
     Cloneable[] queueArray;
-    int index = 0;
-    int maxCapacity;
+    int tail = 0;  //שיניתי שם ל index
+    //int maxCapacity; במקום פשוט אורך המארך
     int head = 0;
+    int queueSize = 0;
 
     /**
      * @param maxCapacity - length of line
      * @throws NegativeCapacityException - if maxCapacitiy is negative
      */
-    public ArrayQueue(int maxCapacity) throws NegativeCapacityException{
+    public ArrayQueue(int maxCapacity) {// לא צריך להצהיר בגלל שזה חריגה בלתי מסומנת יעני runtime
         if(maxCapacity < 0){
             throw new NegativeCapacityException();
         }
-        this.maxCapacity = maxCapacity;
+        //this.maxCapacity = maxCapacity;
         this.queueArray = new Cloneable[maxCapacity];
     }
 
@@ -28,11 +29,14 @@ public class ArrayQueue<E extends Cloneable> implements Queue{
     @Override
     public void enqueue(Cloneable element) throws QueueOverflowException{
         // if null then the queue is empty. if not null then the tail has reached the head
-        if(this.index == this.head && this.queueArray[this.index]!=null){
+
+        if (this.queueSize == queueArray.length){  //במקום אינדקס ולהסתמך על זה שזה לא null
             throw new QueueOverflowException();
         }
-        this.queueArray[index] = element;
-        this.index = (this.index+1)%this.maxCapacity;
+//        if(this.tail == this.head && this.queueArray[this.tail]!=null){}
+        this.queueArray[tail] = element;
+        this.tail = (this.tail +1)%this.queueArray.length;
+        this.queueSize++;
     }
 
     /**
@@ -42,16 +46,22 @@ public class ArrayQueue<E extends Cloneable> implements Queue{
      */
     @Override
     public E dequeue()throws EmptyQueueException{
-        if(this.size() == 0){throw new EmptyQueueException();}
+        if(size() == 0){throw new EmptyQueueException();}
         E element = (E)queueArray[head];
-        queueArray[head] =null;
-        if (head+1==maxCapacity){head=0;} //If we got to the max capacity return head to first cell.
-        else {head++;}//If there is still room in the array, promote head to next.
+        //queueArray[head] =null;    אין צורך כי אנחנו עובדים עם queueSize
+        this.head = (this.head +1)%this.queueArray.length;
+        queueSize--;
+        //if (head+1==queueArray.length){head=0;} //If we got to the max capacity return head to first cell.
+       // else {head++;}//If there is still room in the array, promote head to next.
         return element;
     }
 
     @Override
-    public E peek(){return (E)this.queueArray[0];}
+    public E peek(){
+        if(queueSize ==0 ){
+            throw new EmptyQueueException();
+        }
+        return (E)this.queueArray[head];}
 
     /**
      * calculate queue elements amount, if the index is bigger than the head- calculate the difference
@@ -60,16 +70,31 @@ public class ArrayQueue<E extends Cloneable> implements Queue{
      */
     @Override
     public int size(){
-        if(head == index) return 0;
-        if(head<index){return index-head-1;} //-1 cause index shows the next free spot
-        else{return (index+maxCapacity) - head - 1;} // adding maxCapacity to get the index to be infront
+        return this.queueSize;
+        //לא צריךךךך
+        // if(head == tail) return 0;
+       // if(head< tail){return tail -head-1;} //-1 cause index shows the next free spot
+        //else{return (tail +maxCapacity) - head - 1;} // adding maxCapacity to get the index to be infront
     }
     @Override
-    public boolean isEmpty(){return true;}
+    public boolean isEmpty(){
+        return size()==0;
+    }
     @Override
     public ArrayQueue<E> clone(){return this;}
     @Override
     public Iterator iterator() {
         return null;
     }
+
+    public static class Main {
+        public static void main(String[] args) {
+            MyCloneable mycloneable = new MyCloneable(1);
+            ArrayQueue testqueuearr = new ArrayQueue<MyCloneable>(4);
+            testqueuearr.enqueue(mycloneable);
+
+        }
+    }
+
+
 }
--- src/QueueException.java
+++ src/QueueException.java
@@ -1,2 +1,3 @@
-public class QueueException extends Exception{
+
+public class QueueException extends RuntimeException {
 }
