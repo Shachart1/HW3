c8303acbfad7cf9a1a91d33e1b14ee4b7d583892
MickeyLermanTechnion <102416767+MickeyLermanTechnion@users.noreply.github.com>
2022-06-12 19:18:48 +0300

    mickey

--- src/ToDoListIterator.java
+++ src/ToDoListIterator.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 public class ToDoListIterator implements Iterator{
     LinkedList<Task> list;
     Date dueDate;
+    int index = 0;
 
     public ToDoListIterator(LinkedList list, Date dueDate){
         this.list = list;
@@ -12,8 +13,16 @@ public class ToDoListIterator implements Iterator{
     }
 
     @Override
-    public boolean hasNext(){return false;}
+    public boolean hasNext(){
+        Task current = list.get(index);
+        if(dueDate == null && list.get(index) != null){ //if there is no due date we want to go through all the tasks
+            return true;
+        }
+        return current.getDueDate().compareTo(dueDate) < 0; // if current due date is smaller than provided due date, we want it
+    }
 
     @Override
-    public Task next(){return new Task( "sds", new Date());}
+    public Task next(){
+        index++;
+        return list.get(index-1);}
 }
